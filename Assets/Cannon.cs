0227f9fe5f908af5452fab24bc7e2fad3f341cd5
Mr. Luyk <133881094+MrLuyk@users.noreply.github.com>
2026-03-05 09:55:51 -0500

    reverted back to player script. fixed and froze movement. obstacle spawner. Scene 2.

--- Assets/Cannon.cs
+++ Assets/Cannon.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(Rigidbody))]
 
 public class Cannon : MonoBehaviour
 {
-    Rigidbody rigidbody;
     public GameObject projectilePrefab;
     public Animator animator;
     public Transform fireSocket;
     public float rotationRate = 90.0f;
     public ParticleSystem fireFX;
     public int numProjectiles = 0;
-    public float jumpForce = 10.0f;
 
     // Update is called once per frame
     void Update()
@@ -24,10 +21,6 @@ public class Cannon : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             Fire();
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 
---
+++ Assets/ObstacleSpawner.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    public GameObject obstacleCrownPrefab;
+    public float timeToSpawn;
+    private int obstacleCount = 0;
+    public int obstacleCap = 8;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeToSpawn = Random.Range(1f, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToSpawn -= Time.deltaTime;
+
+        if (timeToSpawn < 0.0f && obstacleCount <= obstacleCap)
+        {
+            SpawnObstacle();
+            timeToSpawn = Random.Range(1f, 2f);
+
+            obstacleCount++;
+        }
+    }
+
+    void SpawnObstacle()
+    {
+        if (obstacleCount < obstacleCap)
+        {
+            Instantiate(obstaclePrefab, transform.position, Quaternion.identity);
+        }
+        if (obstacleCount == obstacleCap)
+        {
+            Instantiate(obstacleCrownPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
