ef57f234a3a7b3ff1e4ebdc2ea96ea00250bf4c9
Mr. Luyk <133881094+MrLuyk@users.noreply.github.com>
2026-03-09 11:50:14 -0400

    Added position reset for parallax layers.

--- Assets/Parallax.cs
+++ Assets/Parallax.cs
@@ -6,12 +6,19 @@ public class Parallax : MonoBehaviour
 {
     public float moveSpeed = 2.0f;
     public Transform[] tiles;
+    public float left = -225f;
+    //public Vector3 right = new Vector3;
 
     void Update()
     {
         for (int i = 0; i < tiles.Length; i++)
         {
             tiles[i].position += Vector3.back * moveSpeed * Time.deltaTime;
+
+            if (tiles[i].position.z <= left)
+            {
+                tiles[i].position = new Vector3(-25f, 0f, 75.9f);
+            }
         }
     }
 }
